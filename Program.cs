269b458123ca62089e48a521486cf4966600d68d
= <=>
2020-06-05 18:07:34 -0300

    Adicionei o programa

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Figgle;
+using Humanizer;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro");
+            string texto = Console.ReadLine();
+            int Numero = Convert.ToInt32(texto);
+            string NumeroPorExtenso = Numero.ToWords();
+            Console.WriteLine($"{Numero} => {NumeroPorExtenso}");
+        }
+    }
+}
