c719887875e85878e277ca6355be593cb2df4990
= <=>
2020-06-05 18:35:06 -0300

    alterei o programa

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace NumeralExtenso
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite um numero inteiro");
+            Console.Write("Digite um numero inteiro: ");
             string texto = Console.ReadLine();
             int Numero = Convert.ToInt32(texto);
             string NumeroPorExtenso = Numero.ToWords();
